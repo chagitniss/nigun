0f81c05a050421bfdc993aa4ded4100be95c7237
chanage <chani1802@gmail.com>
2017-01-19 00:22:02 +0200

    Update app.js

--- NIGUN/client/app.js
+++ NIGUN/client/app.js
@@ -23,6 +23,10 @@ myApp.config(function($routeProvider){
             templateUrl: "views/login.html",
             controller: "contactController"
         })
+         .when('/register', {
+            templateUrl: "views/register.html",
+            controller: "contactController"
+        })
         .when('/dictionary', {
             templateUrl: "views/dictionary.html",
             controller: "contactController"
