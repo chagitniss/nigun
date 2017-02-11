5b0d4693244a94fb3a7f0923516c02be2271dfe0
TaliYa <yazdiy1968@gmail.com>
2017-02-12 01:40:33 +0200

    Update artistsController.js

--- NIGUN/client/controllers/artistsController.js
+++ NIGUN/client/controllers/artistsController.js
@@ -1,5 +1,4 @@
 function artistsController($scope, $http){
-
     $scope.artists={};
     $scope.onLoad=onLoad;
     onLoad();
@@ -15,23 +14,48 @@ console.log("in client ctrl");
                 console.log("Error: " + data);
             });
     }
+
     //===========================================================
-    //search!!----Tali
+    //search  ---- Start
     var contentSearch = document.getElementById("searchInput");
     $scope.contentSearch = contentSearch.value;
 
     $scope.onClickSearch = function(){
-       //alert(contentSearch.value);
-        $http.get('artistsController/loadArtists')
-            .success(function (data) {
-                $scope.artistsSearch = data;
-                console.log("Succeed loading");
-            })
-            .error(function (data) {
-                console.log("Error: " + data);
-            });
+
+        var flag = 0;
+        for(var i = 0; i < $scope.artists.length; i++){
+            //alert($scope.artists[i].name);
+            if($scope.artists[i].name == contentSearch.value){  //if the artist exist
+                flag = 1;
+            }
+        }
+        if(contentSearch.value == "") {
+            alert("נא להזין שם אמן לחיפוש");
+            window.location = "/#";
+            //return false;
+        }
+        else if(flag == 1)  //if the artist exist
+        {
+            $http.get('artistsController/loadArtists')
+                .success(function (data) {
+                    $scope.artistsSearch = data;
+                    console.log("Succeed loading");
+                })
+                .error(function (data) {
+                    console.log("Error: " + data);
+                });
+            $scope.searchInputModel = "";
+            window.location = "#/showSearch";
+        }
+        else{
+            alert("שם אמן לא חוקי. נא להזין שם אמן לחיפוש");
+            $scope.searchInputModel = "";
+            window.location = "/#";
+        }
+
         //alert($scope.artistsSearch[2].name);
     };
+    //search ----- End
     //===========================================================
 }
 
