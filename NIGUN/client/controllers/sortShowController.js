42fef9d9db427a9dd5901411f3a6625c5919cb84
rivkaar <rivkaarm@gmail.com>
2017-02-13 01:24:49 +0200

    char sort work!!

--- NIGUN/client/controllers/sortShowController.js
+++ NIGUN/client/controllers/sortShowController.js
@@ -2,6 +2,7 @@ function sortShowController($scope, $routeParams, $http){
 
     $scope.songs={};
     var name = ($routeParams.name || "");
+    $scope.letter=name;
     console.log("name: " + name);
     $scope.onLoad=onLoad;
     console.log("in sortShowController");
@@ -11,12 +12,23 @@ function sortShowController($scope, $routeParams, $http){
     function onLoad(){
         $http.get('songsController/loadSort/'+name)
             .success(function(data){
-                $scope.songs = data;
-                console.log("Succeed loading");
                 console.log(data);
+               // $scope.songs = data;
+                $scope.songs = filter(data,0,name);
+                console.log("Succeed loading");
+
             })
             .error(function(data){
                 console.log("Error: "+data);
             });
+    }
+
+    function filter(names, index, letter) {
+        var filteredNames = names.filter(function(word) {
+            return word.name.charAt(index) === letter;
+        });
+        if(filteredNames[0]==null)
+            document.getElementById("null").innerHTML = "לא קיימים שירים באות זאת";
+        return filteredNames;
     }
 }
--- NIGUN/client/controllers/typeShowController.js
+++ NIGUN/client/controllers/typeShowController.js
@@ -2,6 +2,7 @@ function typeShowController($scope, $routeParams, $http){
 
     $scope.songs={};
     var name = ($routeParams.name || "");
+    $scope.type=name;
     console.log("name: " + name);
     $scope.onLoad=onLoad;
     console.log("in typeShowController");
@@ -13,6 +14,8 @@ function typeShowController($scope, $routeParams, $http){
             .success(function(data){
                 $scope.songs = data;
                 console.log("Succeed loading");
+                if(data[0]==null)
+                    document.getElementById("null").innerHTML = "לא קיימים שירים מסוג ג'אנר זה";
                 console.log(data);
             })
             .error(function(data){
